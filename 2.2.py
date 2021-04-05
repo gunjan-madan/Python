6449dfeaf9c51a153397f3b2168547e52cc2e86f
Gunjan Madan <>
2021-04-05 01:57:25 +0000

    Added files

---
+++ 2.2.py
@@ -0,0 +1,14 @@
+'''********Task 1: Input*****'''
+print(" ")
+print("*** Task 1:***")
+
+Name=input("Enter your Name: ")
+print("Hello "+Name)
+
+#DIY
+#Take two inputs from user: firstname and Lastname.
+#Print Full name "Hello <firstname> <lastname>"
+
+#Variables value can be changed using code.
+Name ="Batman"
+print(Name)
---
+++ 3.1.py
@@ -0,0 +1,37 @@
+'''*****Task 1:  Basic Arithmetic Operators*****'''
+print(" ")
+print("*** Task 1:***")
+
+addition = 11 +130
+difference = 123-43
+product = 11*12
+division =12/3
+print(addition)
+print(difference)
+print(product)
+print(division)
+
+#DIY
+#add two numbers so that result is 20
+#Subtract two numbers so that result is 20
+#Multiply two numbers so that result is 20
+#Divide two numbers so that result is 20
+
+'''*****Task 2:  More Arithmetic Operators*****'''
+print(" ")
+print("*** Task 2:***")
+# Uncomment the below statements and click Run
+#t=5//3
+#v=7%4
+#w=2**3
+#print(t)
+#print(v)
+#print(w)
+# What did you get? Do you know why? 
+# The // operator divides the 5  by 3 and only gives the integer value, not the remainder
+# The % (modulus) operator gives the remainder when 7 is divided by 4.
+# The ** is used like the exponent operator. So it is read as 2 raised to the power 3
+
+#DIY
+#Use to operators to print Quotient and Remainder when we divide 126 by 3
+#Print 4^5 using the operators
