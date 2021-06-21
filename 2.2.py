c9db3d21088d2a68565e2e35f5a80254b2956b75
Gunjan Madan <53206795+gunjan-madan@users.noreply.github.com>
2021-06-21 14:24:03 +0000

    updated

--- 2.2.py
+++ 2.2.py
@@ -2,13 +2,29 @@
 print(" ")
 print("*** Task 1:***")
 
-Name=input("Enter your Name: ")
-print("Hello "+Name)
+#How to take input from user
+random_input=input()
+print(random_input)
 
+'''********Task 2: Input with questions*****'''
+print(" ")
+print("*** Task 2:***")
+#Take input with proper asks
+# Name=input("Enter your Name: ")
+# print("Hello "+Name)
+
+
+'''********Task 3: Let's input*****'''
+print(" ")
+print("*** Task 3:***")
 #DIY
 #Take two inputs from user: firstname and Lastname.
 #Print Full name "Hello <firstname> <lastname>"
 
+
+'''********Task 4: Change the input*****'''
+print(" ")
+print("*** Task 4:***")
 #Variables value can be changed using code.
-Name ="Batman"
-print(Name)
+# Name ="Batman"
+# print(Name)
