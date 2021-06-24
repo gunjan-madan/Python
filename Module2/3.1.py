3aa163db709f01fa8b453b6059ed780f27962ce1
Gunjan Madan <53206795+gunjan-madan@users.noreply.github.com>
2021-06-24 00:12:53 +0000

    add

--- Module2/3.1.py
+++ Module2/3.1.py
@@ -1,2 +1,13 @@
 # In the previous lesson, you used if-elif-else statements to create a menu based program.
 # Now let us take a look at using nested if-elif statements in creating menu based programs.
+'''Task 1: Nested If-else'''
+print()
+print("*** Task 1: ***")
+print()
+#Make a variable like winning_number and assign any number to it between 1 and 20.
+#Ask user to guess a number between 1 and 20. (Take input)
+#if user guessed correctly, print "YOU WIN"
+#if user didn't guessed correctly then:
+  #if user guessed lower than actual number, then print "TOO LOW"
+   #if user guessed lower than actual number, then print "TOO HIGH"
+
